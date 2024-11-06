13fde49edf3d0d1ba589e5a357498ed29727fa7c
Prudviraj <prudvivemula10@mail.com>
2024-11-06 15:09:56 +0530

    k8 resources

--- workstation.sh
+++ workstation.sh
@@ -59,7 +59,7 @@ ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 VALIDATE $? "kubens installation"
 
 
-Helm
+#Helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
