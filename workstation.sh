89b3524cb9d014d16d911936c6dfde86ec7d5dcf
Prudviraj <prudvivemula10@mail.com>
2024-11-07 11:47:48 +0530

    k8 resources

--- workstation.sh
+++ workstation.sh
@@ -60,9 +60,9 @@ VALIDATE $? "kubens installation"
 
 
 #Helm
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
+sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+sudo chmod 700 get_helm.sh
+sudo ./get_helm.sh
 VALIDATE $? "helm installation"
 
 #k9s
